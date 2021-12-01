8ae02b66e6e8632983f38450f601960e68afa902
josh <github@joshhighet.com>
2021-12-02 12:39:09 +1300

    params

--- azure/appservices-functions/get-all-functions-without-authentication.sh
+++ azure/appservices-functions/get-all-functions-without-authentication.sh
@@ -6,12 +6,12 @@ set -e
 output_filename="functions-noauth.csv"
 
 azfunctions=`az functionapp list --query '[].name' --output tsv`
-functions_count=`echo $azfunctions | wc -w | tr -d ' '`
+functions_count=`echo ${azfunctions} | wc -w | tr -d ' '`
 
 loop_counter=1
 for fn in ${azfunctions}
 do
-    echo "processing $loop_counter of $functions_count - $fn"
+    echo "processing ${loop_counter} of ${functions_count} - ${fn}"
     current_subscription=`az account show --query id --output tsv`
     resource_group=`az functionapp list --query "[?name=='${fn}'].resourceGroup"  -o tsv`
     fn_uri="https://management.azure.com/subscriptions/${current_subscription}/resourceGroups/${resource_group}/providers/Microsoft.Web/sites/${fn}/functions?api-version=2017-08-01"
@@ -20,26 +20,26 @@ do
     # fn_authvalues returns 3 comma separated values - create variable for each
     for authvalue in ${fn_authvalues}
     do
-        auth_level=`echo $authvalue | cut -d',' -f1 | tr -d '"'`
-        auth_type=`echo $authvalue | cut -d',' -f2 | tr -d '"'`
-        invoke_url_template=`echo $authvalue | cut -d',' -f3 | tr -d '"'`
+        auth_level=`echo ${authvalue} | cut -d',' -f1 | tr -d '"'`
+        auth_type=`echo ${authvalue} | cut -d',' -f2 | tr -d '"'`
+        invoke_url_template=`echo ${authvalue} | cut -d',' -f3 | tr -d '"'`
         if [ "$auth_level" == "anonymous" ]
         then
             # check if the function is public by assessing status code of invoke url
             # :enhancement tbd: - check various http verbs
             status_code=`curl -s -o /dev/null -w "%{http_code}" ${invoke_url_template}`
-            if [ "$status_code" != "401" ] && [ "$status_code" != "403" ]
+            if [ "${status_code}" != "401" ] && [ "${status_code}" != "403" ]
             then
-                echo "public function: $fn"
-                echo "auth_level: $auth_level"
-                echo "auth_type: $auth_type"
-                echo "invoke_url_template: $invoke_url_template"
+                echo "public function: ${fn}"
+                echo "auth_level: ${auth_level}"
+                echo "auth_type: ${auth_type}"
+                echo "invoke_url_template: ${invoke_url_template}"
                 # if CSV header does not exist, add it
                 if [ ! -f "functions_without_authentication.csv" ]
                 then
                     echo "name,auth_level,auth_type,invoke_url_template" > ${output_filename}
                 fi
-                echo "$fn,$auth_level,$auth_type,$invoke_url_template" >> ${output_filename}
+                echo "${fn},${auth_level},${auth_type},${invoke_url_template}" >> ${output_filename}
             fi
         fi
     done
