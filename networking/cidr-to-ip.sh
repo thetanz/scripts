f77209f0e90e72f31b693758f4e7615586db9006
josh! <github@joshhighet.com>
2022-01-13 12:55:07 +1300

    add checks

--- networking/cidr-to-ip.sh
+++ networking/cidr-to-ip.sh
@@ -13,6 +13,24 @@
 #downloadable lists of CIDR's by country. (IPv4 & IPv6)
 #https://www.ip2location.com/free/visitor-blocker
 
+# ensure $1 and $2
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "usage: $0 [input cidrs] [output file]"
+    exit 1
+fi
+
+#if $1 not a file
+if [ ! -f "$1" ]; then
+    echo "input 1 must be a file file"
+    exit 1
+fi
+
+# ensure prips in path
+if ! which prips > /dev/null; then
+    echo "prips not found in path"
+    exit 1
+fi
+
 while read line; do
     prips $line >> $2
 done < $1
