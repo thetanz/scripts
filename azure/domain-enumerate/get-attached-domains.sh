2e1a22117d8ca6c71051245cbc1ba517562f875e
josh! <josh.highet@theta.co.nz>
2022-04-06 12:52:31 +1200

    Update get-attached-domains.sh

--- azure/domain-enumerate/get-attached-domains.sh
+++ azure/domain-enumerate/get-attached-domains.sh
@@ -4,9 +4,12 @@
 # https://github.com/Gerenios/AADInternals/blob/fd6474e840f457c32a297cadbad051cabe2a019b/AccessToken_utils.ps1#L1682-L1684
 
 if ! xq --help > /dev/null 2>&1; then
-    echo "xq not found in path"
-    echo "see https://kislyuk.github.io/yq/"
-    exit 1
+    echo "xq not found in path to parse XML, falling back to a best-effort grep/cut/sed"
+    echo "see project https://kislyuk.github.io/yq/"
+    echo ""
+    parsefallback=true
+else
+    parsefallback=false
 fi
 
 if [[ -z "$1" ]]; then
@@ -17,10 +20,17 @@ fi
 export TARGETDOMAIN=${1}
 POST_DATA_XML=$(envsubst < ./template-post.xml)
 
-curl -s -X POST \
+output_xml=$(curl -s -X POST \
 -H 'Content-Type: text/xml; charset=utf-8' \
 -H 'SOAPAction: http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation' \
 -H 'User-Agent: AutodiscoverClient' \
 'https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc' \
---data "${POST_DATA_XML}" \
-| xq '."s:Envelope"."s:Body".GetFederationInformationResponseMessage.Response.Domains.Domain[]' -r
+--data "${POST_DATA_XML}")
+
+if [[ "${parsefallback}" == "false" ]]; then
+  echo "${output_xml}" | xq '."s:Envelope"."s:Body".GetFederationInformationResponseMessage.Response.Domains.Domain[]' -r
+  exit
+else
+  echo "${output_xml}" | sed -e 's_>_&\
+  _g' | grep "</Domain>" | cut -d '<' -f 1
+fi
